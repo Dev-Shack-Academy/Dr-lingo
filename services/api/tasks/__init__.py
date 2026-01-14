0e323adc2899508de5f397b4e86a5f22b9b76f33
Nazeer Ngunga <nazeerngunga021@gmail.com>
2026-01-14 18:30:51 +0200

    workflow

--- services/api/tasks/__init__.py
+++ services/api/tasks/__init__.py
@@ -2,7 +2,7 @@ import logging
 
 from django.conf import settings
 
-from api.events.bus_registry import BusRegistry
+from api.events.bus_registry import BusRegistry  # noqa: E402
 from celery.signals import beat_init, worker_process_init, worker_ready  # noqa: E402
 
 from .assistance_tasks import generate_doctor_assistance_async
